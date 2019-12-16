e427fcee92edbe6b03db5434c5df497fbd5c74dd
Orlov, Dmitry2 <d2.orlov@a1qa.com>
2019-12-16 10:22:18 +0300

    alert tests

--- src/test/java/pajeobject/JavaScriptAlertPage.java
+++ src/test/java/pajeobject/JavaScriptAlertPage.java
@@ -4,13 +4,16 @@ import aquality.selenium.elements.ElementFactory;
 import aquality.selenium.elements.interfaces.ILabel;
 import aquality.selenium.forms.Form;
 import aquality.selenium.logger.Logger;
-import lombok.Getter;
+
 import org.openqa.selenium.By;
 
 public class JavaScriptAlertPage extends Form {
-    @Getter
     private AlertsMenu alertsMenu = new AlertsMenu();
     private ILabel result = new ElementFactory().getLabel(By.id("result"), "result");
+
+    public AlertsMenu getAlertsMenu() {
+        return alertsMenu;
+    }
 
     public String getTextOfResult() {
         Logger.getInstance().info("take text of result");
