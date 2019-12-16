29280d64fd6a8fad12579f9bdcc269a2c98f2ed3
Orlov, Dmitry2 <d2.orlov@a1qa.com>
2019-12-16 12:53:16 +0300

    tutBy

--- src/test/java/testpackage/TestVacancy.java
+++ src/test/java/testpackage/TestVacancy.java
@@ -16,19 +16,16 @@ public class TestVacancy extends BaseTest {
         logger.info("Step 1: Open main page");
         BrowserManager.getBrowser().goTo(PropertiesReader.getValue("START_TUT_BY_PAGE"));
 
-        logger.info("STEP 2:");
+        logger.info("STEP 2: Open work section");
         MainPage mainPage = new MainPage();
-        logger.info("Open work section");
         mainPage.getTopMenuForm().clickBtnByTitle("Работа");
 
-        logger.info("STEP 3:");
+        logger.info(String.format("STEP 3: searchFor %s", query));
         SearchPage searchPage = new SearchPage();
-        logger.info(String.format("STEP 3: \nsearchFor %s", query));
         searchPage.searchFor(query);
 
-        logger.info("STEP 4:");
+        logger.info(String.format("STEP 4: check Results of %s query", query));
         ResultPage resultPage = new ResultPage();
-        logger.info(String.format("check Results of %s query", query));
         Assert.assertTrue(resultPage.checkIsVacancyCorrect(query), "query is not correct");
     }
 
